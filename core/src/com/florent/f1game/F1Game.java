2caddfffe80be4b63d69357a1df2b1d48b4007fc
TaBoOChris <38812859+TaBoOChris@users.noreply.github.com>
2018-05-10 18:11:13 +0200

    test
    Ajout package vehicle

--- core/src/com/florent/f1game/F1Game.java
+++ core/src/com/florent/f1game/F1Game.java
@@ -2,10 +2,8 @@ package com.florent.f1game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class F1Game extends ApplicationAdapter {
---
+++ core/src/com/florent/f1game/vehicle/Body.java
@@ -0,0 +1,30 @@
+package com.florent.f1game.vehicle;
+
+public class Body{
+	
+	private String skin;
+	private int mass;
+	
+	public Body(String s, int m) {
+		this.setSkin(s);
+		this.setMass(m);
+	}
+
+	public String getSkin() {
+		return skin;
+	}
+
+	public void setSkin(String skin) {
+		this.skin = skin;
+	}
+
+	public int getMass() {
+		return mass;
+	}
+
+	public void setMass(int mass) {
+		this.mass = mass;
+	}
+	
+	
+}
